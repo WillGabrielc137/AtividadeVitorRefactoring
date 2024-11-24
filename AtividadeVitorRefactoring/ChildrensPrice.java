d920e8fe62dcdde67f925698a118a6b397233e2c
WillGabrielc137 <willysmgabrielc137@gmail.com>
2024-11-24 16:01:16 -0300

    Commit 13

--- AtividadeVitorRefactoring/ChildrensPrice.java
+++ AtividadeVitorRefactoring/ChildrensPrice.java
@@ -8,10 +8,10 @@ public class ChildrensPrice extends Price {
 
     @Override
     public double getCharge(int daysRented) {
-        double amount = 1.5;
+        double result = 1.5;
         if (daysRented > 3) {
-            amount += (daysRented - 3) * 1.5;
+            result += (daysRented - 3) * 1.5;
         }
-        return amount;
+        return result;
     }
 }
--- AtividadeVitorRefactoring/NewReleasePrice.java
+++ AtividadeVitorRefactoring/NewReleasePrice.java
@@ -15,5 +15,4 @@ public class NewReleasePrice extends Price {
     public int getFrequentRenterPoints(int daysRented) {
         return (daysRented > 1) ? 2 : 1;
     }
-
 }
